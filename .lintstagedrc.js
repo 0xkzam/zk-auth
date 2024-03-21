3fb5b5633290e09c7247c7a08d736de8653cb4ae
0xkzam <0xkzhunter@gmail.com>
2024-03-22 01:45:02 +0530

    missed config files

---
+++ .lintstagedrc.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const buildNextEslintCommand = (filenames) =>
+  `yarn next:lint --fix --file ${filenames
+    .map((f) => path.relative(path.join("packages", "nextjs"), f))
+    .join(" --file ")}`;
+
+const checkTypesNextCommand = () => "yarn next:check-types";
+
+const buildHardhatEslintCommand = (filenames) =>
+  `yarn hardhat:lint-staged --fix ${filenames
+    .map((f) => path.relative(path.join("packages", "hardhat"), f))
+    .join(" ")}`;
+
+const buildNoirEslintCommand = (filenames) =>
+  `yarn noir:lint-staged --fix ${filenames
+    .map((f) => path.relative(path.join("packages", "noir"), f))
+    .join(" ")}`;
+
+module.exports = {
+  "packages/nextjs/**/*.{ts,tsx}": [
+    buildNextEslintCommand,
+    checkTypesNextCommand,
+  ],
+  "packages/hardhat/**/*.{ts,tsx}": [buildHardhatEslintCommand],
+  "packages/noir/**/*.{ts,tsx}": [buildNoirEslintCommand],
+};
