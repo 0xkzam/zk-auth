413c638747e9453a028918973f1bebe3d6b7d94f
kartikjn <kj@rapidinnovation.dev>
2024-03-28 14:34:23 +0530

    fix: assest required quantity

--- packages/noir/scripts/export_circuits_src.ts
+++ packages/noir/scripts/export_circuits_src.ts
@@ -10,6 +10,26 @@ function getData(project: string) {
   try {
     console.log("🤓 trying to read:", path);
     const code = JSON.parse(readFileSync(path, "utf8") as string);
+
+    const keys = Object.keys(code);
+    for (const key of keys) {
+      if (key === "abi") {
+        const subKeys = Object.keys(code[key]);
+        for (const subKey of subKeys) {
+          if (subKey === "param_witnesses") {
+            const finalKeys = Object.keys(code[key][subKey]);
+            for (const finalKey of finalKeys) {
+              const data = code[key][subKey][finalKey][0];
+              code[key][subKey][finalKey] = Array.from(
+                { length: data.end - data.start },
+                (_, index) => index + data.start + 1,
+              );
+            }
+          }
+        }
+      }
+    }
+
     return code;
   } catch (err: any) {
     console.error(`❌ error when reading file (${path}) with error: ${err.stack}\n`);
